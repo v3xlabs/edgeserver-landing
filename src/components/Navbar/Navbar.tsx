324315411e61d9dc79f217d9c4ef0d0a7ad52d6a
Lucemans <luc@lucemans.nl>
2022-05-29 16:46:34 +0200

    Initial Commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import icon from 'url:../../../assets/signal.svg';
+
+import { NavbarLink } from './NavLink';
+import { ProjectSelector } from './ProjectSelector';
+
+const links: {
+    name: string;
+    path: string;
+}[] = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Projects',
+        path: '/projects',
+    },
+];
+
+export const Navbar: FC = () => {
+    return (
+        <div
+            className="w-full p-4 pb-0
+            bg-black-900 border-b-2 border-neutral-700"
+        >
+            <div className="w-full max-w-4xl mx-auto flex flex-col gap-2">
+                <div className="flex gap-4 container mx-auto w-full">
+                    <img src={icon} alt="Signal Icon" className="flex-0" />
+                    <ProjectSelector />
+                    <div className="ml-auto">Hello</div>
+                </div>
+
+                <div className="flex container mx-auto w-full justify-between">
+                    <div className="flex gap-4">
+                        {links.map((link) => (
+                            <NavbarLink name={link.name} path={link.path} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const Home: FC = () => {
+    return <div className="m-8">Home</div>;
+};
