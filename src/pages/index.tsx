846143e9e93f61151c13947ac92515066e669b5a
Lucemans <luc@lucemans.nl>
2022-05-29 17:25:20 +0200

    Introduce Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 
 export const Home: FC = () => {
-    return <div className="m-8">Home</div>;
+    return (
+        <div className="m-8 flex justify-center text-center">
+            Welcome to Signal Edge, this page is still under construction.
+        </div>
+    );
 };
