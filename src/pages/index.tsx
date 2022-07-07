bb3429049be855f3a373c43bd21fb4a41e7bf5d2
Jakob Helgesson <jakob.helgesson@gmail.com>
2022-07-07 22:22:38 +0200

    Revert "New Navbar (#1)" (#3)
    This reverts commit fcbb2587fb87400c82257db0684f231612bdb7e2.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
-import { Navbar } from "@components/Navbar";
-import { NextSeo } from "next-seo";
-import { FC } from "react";
+import { FC } from 'react';
 
-const Home: FC = () => {
+export const Home: FC = () => {
     return (
-        <>
-            <NextSeo
-                title="Web3 Hosting - EdgeServer"
-                description="Easily deploy your projects."
-            />
-            <Navbar></Navbar>
-        </>
+        <div>
+            <div className="w-full flex justify-center items-center h-96">
+                <div className="text-6xl font-bold max-w-2xl text-center">
+                    The Fastest{' '}
+                    <span className="text-transparent bg-clip-text bg-gradient-to-tr from-purple-400 to-pink-600">
+                        Web3
+                    </span>{' '}
+                    <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-400 to-cyan-600">
+                        Edge
+                    </span>{' '}
+                    Server
+                </div>
+            </div>
+            <div className="m-8 flex justify-center text-center">
+                Just like netlify and vercel, but open-source, decentralized,
+                and modular.
+            </div>
+            <div className="m-8 flex justify-center text-center">
+                Welcome to @edgeserverio, this page is still under construction.
+            </div>
+        </div>
     );
 };
-
-export default Home;
